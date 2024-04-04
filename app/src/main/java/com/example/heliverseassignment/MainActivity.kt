8d32a7ce48c2b4af73491849512d4df52421524a
runDMC13 <98306603+runDMC13@users.noreply.github.com>
2024-04-04 17:55:59 +0530

    First Commit

---
+++ app/src/main/java/com/example/heliverseassignment/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.example.heliverseassignment
+
+import android.os.Build
+import android.os.Bundle
+import android.view.WindowInsets
+import android.view.WindowInsetsController
+import android.view.WindowManager
+import androidx.appcompat.app.AppCompatActivity
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        supportActionBar?.hide()
+
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
+
+        setContentView(R.layout.activity_main)
+    }
+}
